f0a8f0ea463ad3ccb9c89261d9395c527c88e480
Scott Rocca <scottrocca@gmail.com>
2017-09-18 21:37:25 -0700

    Initial Commit

---
+++ ToolBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToolBox
+//
+//  Created by Scott Rocca on 9/18/17.
+//  Copyright © 2017 ScottRocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToolBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToolBox
+//
+//  Created by Scott Rocca on 9/18/17.
+//  Copyright © 2017 ScottRocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
